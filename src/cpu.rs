63bf8dd3f4f5f5142e182d8af0d7351b32063952
Ben Simms <ben@bensimms.moe>
2018-01-19 11:00:18 +0000

    Clean: modularise

---
+++ src/cpu.rs
@@ -0,0 +1,112 @@
+use std::ops::{Index, IndexMut};
+
+
+pub type Reg = u64;
+
+pub type CpuIndex = u16;
+
+pub trait CpuIndexable {
+    fn register(&self) -> bool;
+    fn deref(&self) -> bool;
+    fn strip_register(&self) -> Self;
+    fn strip_deref(&self) -> Self;
+    fn index(&self) -> usize;
+
+    fn make_index(pos: u16, reg: bool, deref: bool) -> Self;
+}
+
+impl CpuIndexable for CpuIndex {
+    fn register(&self) -> bool {
+        self & (1 << 15) != 0
+    }
+
+    fn deref(&self) -> bool {
+        self & (1 << 14) != 0
+    }
+
+    fn strip_register(&self) -> CpuIndex {
+        self & !(1 << 15)
+    }
+
+    fn strip_deref(&self) -> CpuIndex {
+        self & !(1 << 14)
+    }
+
+    fn index(&self) -> usize {
+        self.strip_register().strip_deref() as usize
+    }
+
+    fn make_index(pos: u16, reg: bool, deref: bool) -> CpuIndex {
+        pos | (reg as CpuIndex) << 15 | (deref as CpuIndex) << 14
+    }
+}
+
+pub struct Cpu {
+    pub mem: Vec<u8>,
+    pub regs: RegBlock,
+    pub flags: CpuFlags,
+    pub cycles: u64,
+}
+
+impl Cpu {
+    pub fn new(memsize: usize, regcount: usize) -> Cpu {
+        Cpu {
+            mem: vec![0; memsize],
+            regs: RegBlock::new(regcount),
+            flags: CpuFlags::default(),
+            cycles: 0,
+        }
+    }
+}
+
+pub struct RegBlock {
+    stk: Reg,
+    bas: Reg,
+    cur: Reg,
+    regs: Vec<Reg>,
+}
+
+impl RegBlock {
+    fn new(size: usize) -> RegBlock {
+        RegBlock {
+            stk: 0,
+            bas: 0,
+            cur: 0,
+            regs: vec![0; size],
+        }
+    }
+}
+
+impl Index<usize> for RegBlock {
+    type Output = Reg;
+
+    fn index(&self, index: usize) -> &Reg {
+        match index {
+            0 => &self.stk,
+            1 => &self.bas,
+            2 => &self.cur,
+            x => &self.regs[x],
+        }
+    }
+}
+
+impl IndexMut<usize> for RegBlock {
+    fn index_mut(&mut self, index: usize) -> &mut Reg {
+        match index {
+            0 => &mut self.stk,
+            1 => &mut self.bas,
+            2 => &mut self.cur,
+            x => &mut self.regs[x],
+        }
+    }
+}
+
+bitflags! {
+    #[derive(Default)]
+    pub struct CpuFlags: u8 {
+        const RUNNING = 0b0000001;
+        const SIGNED  = 0b0000010;
+        const ZERO    = 0b0000100;
+        const LE      = 0b0001000;
+    }
+}
---
+++ src/memory.rs
@@ -0,0 +1,120 @@
+use byteorder::{ByteOrder, NativeEndian};
+use num::FromPrimitive;
+
+use cpu::{Cpu, CpuIndex, Reg, CpuIndexable};
+
+#[derive(Copy, Clone)]
+pub enum MemSize {
+    U1,
+    U2,
+    U4,
+    U8,
+}
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub enum MemReg {
+    U1(u8),
+    U2(u16),
+    U4(u32),
+    U8(u64),
+}
+
+impl MemSize {
+    pub fn len(self) -> usize {
+        match self {
+            MemSize::U1 => 1,
+            MemSize::U2 => 2,
+            MemSize::U4 => 4,
+            MemSize::U8 => 8,
+        }
+    }
+
+    pub fn pack(self, val: u64) -> MemReg {
+        match self {
+            MemSize::U1 => MemReg::U1(val as u8),
+            MemSize::U2 => MemReg::U2(val as u16),
+            MemSize::U4 => MemReg::U4(val as u32),
+            MemSize::U8 => MemReg::U8(val as u64),
+        }
+    }
+}
+
+impl MemReg {
+    pub fn len(self) -> usize {
+        match self {
+            MemReg::U1(_) => 1,
+            MemReg::U2(_) => 2,
+            MemReg::U4(_) => 4,
+            MemReg::U8(_) => 8,
+        }
+    }
+
+    pub fn size(self) -> MemSize {
+        match self {
+            MemReg::U1(_) => MemSize::U1,
+            MemReg::U2(_) => MemSize::U2,
+            MemReg::U4(_) => MemSize::U4,
+            MemReg::U8(_) => MemSize::U8,
+        }
+    }
+
+    pub fn unpack<N: FromPrimitive>(self) -> N {
+        match self {
+            MemReg::U1(x) => N::from_u8(x).unwrap(),
+            MemReg::U2(x) => N::from_u16(x).unwrap(),
+            MemReg::U4(x) => N::from_u32(x).unwrap(),
+            MemReg::U8(x) => N::from_u64(x).unwrap(),
+        }
+    }
+}
+
+
+impl Cpu {
+    pub fn read_memory(&self, size: MemSize, index: usize) -> MemReg {
+        let range = index .. (index + size.len());
+        match size {
+            MemSize::U1 => MemReg::U1(self.mem[index]),
+            MemSize::U2 => MemReg::U2(NativeEndian::read_u16(&self.mem[range])),
+            MemSize::U4 => MemReg::U4(NativeEndian::read_u32(&self.mem[range])),
+            MemSize::U8 => MemReg::U8(NativeEndian::read_u64(&self.mem[range])),
+        }
+    }
+
+    pub fn write_memory(&mut self, mem: MemReg, index: usize) {
+        let range = index .. (index + mem.len());
+        match mem {
+            MemReg::U1(x) => self.mem[index] = x,
+            MemReg::U2(x) => NativeEndian::write_u16(&mut self.mem[range], x),
+            MemReg::U4(x) => NativeEndian::write_u32(&mut self.mem[range], x),
+            MemReg::U8(x) => NativeEndian::write_u64(&mut self.mem[range], x),
+        }
+    }
+
+    pub fn write(&mut self, dat: MemReg, to: CpuIndex) {
+        if to.deref() {
+            let to = self.read(dat.size(), to.strip_deref()).unpack();
+            self.write_memory(dat, to);
+        } else {
+            if to.register() {
+                let val: Reg = dat.unpack();
+                self.regs[to.index()] = val;
+            } else {
+                self.write_memory(dat, to.index());
+            }
+        }
+    }
+
+    pub fn read(&self, size: MemSize, index: CpuIndex) -> MemReg {
+        let val = if index.register() {
+            self.regs[index.index()] as u64
+        } else {
+            index.index() as u64
+        };
+
+        if index.deref() {
+            self.read_memory(size, val as usize)
+        } else {
+            size.pack(val)
+        }
+    }
+}
